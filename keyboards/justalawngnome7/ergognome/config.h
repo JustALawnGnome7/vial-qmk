56dcf8c5a5fe61e60fd4ebe83408bc1d173175c1
Miles Ramage <miles.ramage@yahoo.com>
2024-04-02 11:12:45 -0500

    Decreased max brightness.

--- keyboards/justalawngnome7/ergognome/config.h
+++ keyboards/justalawngnome7/ergognome/config.h
@@ -43,7 +43,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   #define RGB_MATRIX_SLEEP // turn off effects when suspended
   #define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
   #define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-  #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 185 // OK: 175, ERROR: 190; limits maximum brightness of LEDs. If not defined maximum brightness is set to 255.
+  #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 175 // OK: 185, ERROR: 190; limits maximum brightness of LEDs. If not defined maximum brightness is set to 255.
 
   #define RGB_MATRIX_DEFAULT_ON true
   #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_BREATHING //RGB_MATRIX_SOLID_COLOR
@@ -78,13 +78,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   //#define ENABLE_RGB_MATRIX_RAINDROPS
   //#define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
   #define ENABLE_RGB_MATRIX_HUE_BREATHING
-  //#define ENABLE_RGB_MATRIX_HUE_PENDULUM
-  //#define ENABLE_RGB_MATRIX_HUE_WAVE
+  #define ENABLE_RGB_MATRIX_HUE_PENDULUM
+  #define ENABLE_RGB_MATRIX_HUE_WAVE
   //#define ENABLE_RGB_MATRIX_PIXEL_FRACTAL
   //#define ENABLE_RGB_MATRIX_PIXEL_FLOW
   //#define ENABLE_RGB_MATRIX_PIXEL_RAIN
   #define ENABLE_RGB_MATRIX_STARLIGHT
-  #define ENABLE_RGB_MATRIX_STARLIGHT_DUAL_HUE
+  //#define ENABLE_RGB_MATRIX_STARLIGHT_DUAL_HUE
   #define ENABLE_RGB_MATRIX_STARLIGHT_DUAL_SAT
   #define ENABLE_RGB_MATRIX_RIVERFLOW
 #endif
